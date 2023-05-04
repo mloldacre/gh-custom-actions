98d931fbc9720f6554898cc7d6dfba6680fc0651
Michael Oldacre <65476228+mloldacre@users.noreply.github.com>
2023-05-03 21:43:37 -0400

    inputs set up in action.yml

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+	// 1) Get input values
+
 	core.notice('Hello from my custom javascript action!');
 }
 
